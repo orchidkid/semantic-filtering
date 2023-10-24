a0ddb56c19bb82ddcf252618bf1ab17ca899a40c
orchidkid <78425720+orchidkid@users.noreply.github.com>
2023-10-24 14:48:49 +0300

    New

--- script.js
+++ script.js
@@ -1,77 +1,133 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Обработчики событий
-    document.getElementById('loadKeywords').addEventListener('click', loadKeywords);
-    document.getElementById('highlightMinusWordsBtn').addEventListener('click', highlightSelectedWords);
-    document.getElementById('filterKeywordsBtn').addEventListener('click', filterKeywords);
-    document.getElementById('downloadCSVBtn').addEventListener('click', downloadCSV);
+
+// JavaScript код тут
+
+const phrasesInput = document.getElementById('phrases');
+const loadButton = document.getElementById('loadButton');
+const phrasesOutput = document.getElementById('phrasesOutput');
+
+const minusWordInput = document.getElementById('minusWord');
+const minusWordType = document.getElementById('minusWordType');
+const addButton = document.getElementById('addButton');
+const minusWordsOutput = document.getElementById('minusWordsOutput');
+const minusWords = [];
+
+const highlightButton = document.getElementById('highlightButton');
+const filteredPhrases = [];
+const removedPhrases = [];
+
+const filterButton = document.getElementById('filterButton');
+const filteredOutput = document.getElementById('filteredOutput');
+const removedOutput = document.getElementById('removedOutput');
+
+const exportButton = document.getElementById('exportButton');
+
+loadButton.addEventListener('click', () => {
+    phrasesOutput.innerHTML = phrasesInput.value.split('\n').map(line => `<span class = "added">${line.toLowerCase()}</span>`).join('');
 });
 
-function loadKeywords() {
-    const keywordInput = document.getElementById('keywordsInput').value;
-    const keywordsList = keywordInput.split('\n');
-    const displayArea = document.getElementById('keywordsDisplay');
-    displayArea.innerHTML = '';
+addButton.addEventListener('click', () => {
+    const minusWord = minusWordInput.value;
+    const type = minusWordType.value;
+    minusWords.push({ word: minusWord, type });
+    updateMinusWordsOutput();
+    clearMinusWord(); // Очищення поля введення мінус-слова
+});
 
-    keywordsList.forEach(phrase => {
-        const div = document.createElement('div');
-        phrase.split(' ').forEach(word => {
-            const span = document.createElement('span');
-            span.innerText = word;
-            span.classList.add('keyword');
-            span.addEventListener('click', () => toggleKeywordSelection(word));
-            div.appendChild(span);
+highlightButton.addEventListener('click', () => {
+    const phrases = phrasesInput.value.split('\n');
+    filteredPhrases.length = 0;
+    removedPhrases.length = 0;
+    phrases.forEach((phrase) => {
+        const highlighted = minusWords.some(({ word, type }) => {
+            if (type === 'Часткове входження' && phrase.includes(word)) {
+                return true;
+            } else if (type === 'Містить тільки дане слово' && word === phrase) {
+                return true;
+            } else if (type === 'Фразове входження' && phrase.includes(word)) {
+                return true;
+            }
+            return false;
         });
-        displayArea.appendChild(div);
-    });
-}
 
-function toggleKeywordSelection(word) {
-    const minusWordsArea = document.getElementById('minusWords');
-    let currentMinusWords = splitByNewline(minusWordsArea.value);
-
-    if (currentMinusWords.includes(word)) {
-        currentMinusWords = currentMinusWords.filter(w => w !== word);
-    } else {
-        currentMinusWords.push(word);
-    }
-
-    minusWordsArea.value = currentMinusWords.join('\n');
-    highlightSelectedWords();
-}
-
-function highlightSelectedWords() {
-    const minusWords = splitByNewline(document.getElementById('minusWords').value);
-    const keywords = document.querySelectorAll('.keyword');
-    
-    keywords.forEach(kw => {
-        const keywordText = kw.innerText.toLowerCase(); // Приводим текст ключевого слова к нижнему регистру
-        const shouldHighlight = minusWords.some(mw => keywordText.includes(mw.toLowerCase())); // Проверяем вхождение
-        if (shouldHighlight) {
-            kw.classList.add('highlighted');
+        if (highlighted) {
+            removedPhrases.push(phrase);
         } else {
-            kw.classList.remove('highlighted');
+            filteredPhrases.push(phrase);
         }
     });
+    updatePhrasesOutput();
+});
+
+filterButton.addEventListener('click', () => {
+    updateFilteredOutput();
+    updateRemovedOutput();
+});
+
+exportButton.addEventListener('click', () => {
+    exportToCsv();
+});
+
+function updateMinusWordsOutput() {
+    const outputHtml = minusWords.map(({ word, type }, index) => {
+        return `
+          <div class="minus-word-container">
+            ${word} (${type})
+            <div class="remove-button-container"><span class="remove-button" data-index="${index}" onclick="removeMinusWord(${index})">✖</span></div>
+          </div>`;
+    }).join('');
+    minusWordsOutput.innerHTML = outputHtml;
 }
 
-function filterKeywords() {
-    const keywords = splitByNewline(document.getElementById('keywordsInput').value);
-    const minusWords = splitByNewline(document.getElementById('minusWords').value);
-    const filtered = keywords.filter(kw => !minusWords.some(mw => kw.includes(mw)));
-    document.getElementById('filteredKeywords').value = filtered.join('\n');
+function updatePhrasesOutput() {
+    const outputHtml = phrasesInput.value.split('\n').map((phrase) => {
+        if (removedPhrases.includes(phrase)) {
+            return `<span class="removed">${phrase}</span>`;
+        } else if (filteredPhrases.includes(phrase)) {
+            return `<span class="highlighted">${phrase}</span>`;
+        }
+        return `<span>${phrase}</span>`;
+    }).join('');
+    phrasesOutput.innerHTML = outputHtml;
 }
 
-function downloadCSV() {
-    const filteredKeywords = splitByNewline(document.getElementById('filteredKeywords').value);
-    let csvContent = "data:text/csv;charset=utf-8,Ключові слова\n" + filteredKeywords.join('\n');
-    let encodedUri = encodeURI(csvContent);
-    let link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "filtered_keywords.csv");
-    document.body.appendChild(link);
-    link.click();
+function updateFilteredOutput() {
+    const outputHtml = filteredPhrases.map((phrase) => `<span>${phrase}</span>`).join('');
+    filteredOutput.innerHTML = `Відфільтровані фрази:<br>${outputHtml}`;
 }
 
-function splitByNewline(str) {
-    return str.split('\n').filter(line => line.trim() !== '');
+function updateRemovedOutput() {
+    const outputHtml = removedPhrases.map((phrase) => `<span>${phrase}</span>`).join('');
+    removedOutput.innerHTML = `Видалені фрази:<br>${outputHtml}`;
+}
+
+function removeMinusWord(index) {
+    if (index >= 0 && index < minusWords.length) {
+        minusWords.splice(index, 1);
+        updateMinusWordsOutput();
+    }
+}
+
+function clearMinusWord() {
+    minusWordInput.value = '';
+}
+
+function exportToCsv() {
+    const csvData = [];
+    const maxLen = Math.max(filteredPhrases.length, removedPhrases.length);
+    for (let i = 0; i < maxLen; i++) {
+        const filteredPhrase = i < filteredPhrases.length ? filteredPhrases[i] : '';
+        const removedPhrase = i < removedPhrases.length ? removedPhrases[i] : '';
+        csvData.push([filteredPhrase, removedPhrase]);
+    }
+
+    const csvContent = csvData.map((row) => row.join(',')).join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'filtered_phrases.csv';
+    a.click();
+
+    URL.revokeObjectURL(url);
 }
